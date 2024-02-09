6931807f344a54e9647840b80e871cfaad0b3b1f
Thamir <thamirsiddik@gmail.com>
2024-02-09 16:28:45 +0530

    MongoDB Database Setupped

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('MongoDB Database Connected');
+}).catch((error) => {
+    console.log('MongoDB Database Connection Error', error);
+})
 const app =express();
 const port = 3000;
 
