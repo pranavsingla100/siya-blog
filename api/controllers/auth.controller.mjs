ff37cca01098ed9054d666adb6822deebdfcd6fa
Thamir <thamirsiddik@gmail.com>
2024-02-12 21:57:08 +0530

    Created signin API route

--- api/controllers/auth.controller.mjs
+++ api/controllers/auth.controller.mjs
@@ -1,26 +1,64 @@
-import User from '../models/user.model.mjs'
-import bcryptjs from 'bcryptjs'
-import { errorHandler } from '../utils/error.mjs';
+import User from "../models/user.model.mjs";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.mjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-    const {userName, email, password } = req.body;
+  const { username, email, password } = req.body;
+
+  // Check if any required field is missing or empty
+  if (!username || !email || !password || username.trim() === "" || email.trim() === "" || password.trim() === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    // Hash the password asynchronously
+    const hashedPassword = await bcryptjs.hashSync(password, 10);
+
+    // Create a new user instance
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save the new user to the database
+    await newUser.save();
+
+    // Send success response
+    res.json({ message: "Signup successful" });
+  } catch (error) {
+    // Pass any error to the error handling middleware
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fiels are required"));
+  }
+
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) {
+      return next(errorHandler(400, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validuser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid username or password"));
+    }
+
+    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validuser._doc;
     
-    if(!userName || !email || !password || userName === '' || email === '' || password === ''){
-        next(errorHandler(400, 'All fields are required'));
-    }
-
-    const hashedPassword = await bcryptjs.hashSync(password, 10)
-
-    const newUser = User({
-        userName,
-        email,
-        password: hashedPassword
-    })
-
-    try{
-      await newUser.save();
-      res.json({message : 'Signup successful'})
-    }catch(error){
-      next(error);
-    }
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
