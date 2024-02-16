813350ffa46ec32675649da1eb54a8ebf83cfe96
Thamir <thamirsiddik@gmail.com>
2024-02-16 15:40:27 +0530

    Implemented user account deletion funtionality

--- api/controllers/user.controller.mjs
+++ api/controllers/user.controller.mjs
@@ -54,7 +54,7 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
+        
 export const  deleteUser = async(req, res, next) => {
   if(req.user.id != req.params.userId){
     next(errorHandler(403, 'You are not allowed to delete this account.'));
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,6 +1,7 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
 import {
   getDownloadURL,
   getStorage,
@@ -14,16 +15,20 @@ import {
   updateStart,
   updateSuccess,
   updateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure
 } from "../redux/user/userSlice.mjs";
 
 export default function DashProfile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, error } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileurl, setImageFileurl] = useState(null);
   const [formData, setFormData] = useState({});
   const [imageFileUploading, setImageFileUploading] = useState(false);
   const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
   const [updateUserError, setUpdateUserError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
   const [imageFileUploadingProgress, setImageFileUploadingProgress] =
     useState(null);
@@ -115,6 +120,26 @@ export default function DashProfile() {
       setUpdateUserError(error.message);
     }
   };
+
+  const handleDeleteUser = async() => {
+    setShowModal(false);
+    try{
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      })
+
+      const data = await res.json();
+      if(!res.ok){
+        dispatch(deleteUserFailure());
+      }else{
+        dispatch(deleteUserSuccess());
+      }
+    }catch(error){
+      console.error(error);
+      dispatch(deleteUserFailure());
+    }
+  }
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -189,7 +214,7 @@ export default function DashProfile() {
         </Button>
       </form>
       <div className="flex justify-between text-red-500 mt-5">
-        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer" onClick={()=>setShowModal(true)}>Delete Account</span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
       {updateUserSuccess && (
@@ -202,6 +227,28 @@ export default function DashProfile() {
           {updateUserError}
         </Alert>
       )}
+      {error && (
+        <Alert color='failure' className="mt-5">
+          {error}
+        </Alert>
+      )}
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size={'md'}>
+        <Modal.Header/>
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto"/>
+              <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete your account?</h3>
+              <div className="flex justify-center gap-4">
+                <Button color="failure" onClick={handleDeleteUser}>
+                  Yes, I'm sure
+                </Button>
+                <Button color="gray" onClick={() => setShowModal(false)}>
+                  No, Cancel
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+      </Modal>
     </div>
   );
 }
