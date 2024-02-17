2670b33b6b45b68fbebdac2889baa364b1ed907e
Thamir <thamirsiddik@gmail.com>
2024-02-17 16:30:36 +0530

    Created get post API route

--- api/routes/post.route.mjs
+++ api/routes/post.route.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.mjs';
-import { createBlog } from '../controllers/post.controller.mjs';
+import { createBlog, getPosts } from '../controllers/post.controller.mjs';
 
 const router = express.Router();
 
-router.post('/create-post', verifyToken, createBlog)
+router.post('/create-post', verifyToken, createBlog);
+router.get('/get-post', getPosts);
 
 export default router;
