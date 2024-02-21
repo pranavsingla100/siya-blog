e07428376db3339011011907997d72b0c81ae490
Thamir <thamirsiddik@gmail.com>
2024-02-21 22:32:52 +0530

    Created get all users API route

--- api/routes/user.route.mjs
+++ api/routes/user.route.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser, deleteUser, signOut } from "../controllers/user.controller.mjs";
+import { test, updateUser, deleteUser, signOut, getUsers } from "../controllers/user.controller.mjs";
 import { verifyToken } from "../utils/verifyUser.mjs";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/test', test)
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signOut);
+router.get('/get-users', verifyToken, getUsers);
 
 export default router;
