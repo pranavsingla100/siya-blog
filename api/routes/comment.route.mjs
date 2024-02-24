55126ed48819f5172367bda42f082ceeba4523fd
Thamir <thamirsiddik@gmail.com>
2024-02-24 11:18:04 +0530

    Show the comments of a post

--- api/routes/comment.route.mjs
+++ api/routes/comment.route.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.mjs'
-import { createComment } from '../controllers/comment.controller.mjs';
+import { createComment, getPostComments } from '../controllers/comment.controller.mjs';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment)
+router.get('/get-post-comments/:postId', verifyToken, getPostComments);
 
 export default router;
--- api/routes/user.route.mjs
+++ api/routes/user.route.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser, deleteUser, signOut, getUsers } from "../controllers/user.controller.mjs";
+import { test, updateUser, deleteUser, signOut, getUsers, getUser } from "../controllers/user.controller.mjs";
 import { verifyToken } from "../utils/verifyUser.mjs";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signOut);
 router.get('/get-users', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
