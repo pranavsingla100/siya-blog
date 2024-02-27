10a07c9f4686c3f8a69946d6dca0f70de1b80db3
Thamir <thamirsiddik@gmail.com>
2024-02-27 10:26:30 +0530

    Completed home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("/api/post/get-post");
+        if (res.ok) {
+          const data = await res.json();
+          setPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return loading ? (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="spinner"></div>
+    </div>
+  ) : (
+    <div>
+      <div className="flex flex-col gap-6 p-8 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <p className="text-gray-500 text-sm">
+          Here you'll find a variety of articles and tutorials on topics such as
+          web development, software engineering, and programming languages.
+        </p>
+        <Link
+          to="/search"
+          className="text-sm text-teal-500 font-bold hover:underline"
+        >
+          View All Posts
+        </Link>
+      </div>
+
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+
+      <div className="mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center mb-4">
+              Recent Posts
+            </h2>
+            <div className="flex flex-wrap gap-4 justify-center">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 text-center hover:underline"
+            >
+              View All Posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
